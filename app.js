2e689278a4a13b7ac28f52c6ffdb09e0bf386c40
Subramanyam S <shastri9999@gmail.com>
2015-05-19 20:37:08 +0530

    Adding views and angular bootstrap files

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require('express');
-var app    = express();
+var app     = express();
+var hbs     = require('express-handlebars');
 
+app.engine('handlebars', hbs({defaultLayout: 'main'}));
+app.set('views', './views');
+app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
 app.get('/', function(request, response){
-	response.send('OK');
+	response.render('index');
 });
 
-var server = app.listen(process.env.PORT || 5000, function(){
-	
-});
+var server = app.listen(process.env.PORT || 5000);
---
+++ public/app/main.js
@@ -0,0 +1,1 @@
+angular.module('InventoryApp', ['ngMaterial','ngRoute']);
